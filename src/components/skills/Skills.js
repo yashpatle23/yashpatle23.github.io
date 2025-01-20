a3c55c88cade01444f0839403ccd99e3cf20d0d4
yash patle <105612209+yashpatle23@users.noreply.github.com>
2025-01-20 13:37:48 +0530

    gg

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,64 @@
+import React, { useRef } from 'react'
+import IsVisible from 'react-is-visible'
+import { Fade } from 'react-reveal'
+import skillsData from '../../data/skills.json'
+import { useContainerDimensions } from '../../hooks'
+
+const Skills = () => {
+  const skillsWrapper = useRef()
+  const { width } = useContainerDimensions(skillsWrapper)
+  const { skills } = skillsData
+
+  return (
+    <Fade duration={1000}>
+      <div style={{ position: 'relative', width: '100%', maxWidth: 600 }}>
+        <IsVisible once>
+          {(isVisibleSkillsWrapper) => (
+            <div
+              className="skills-wrapper"
+              style={
+                isVisibleSkillsWrapper
+                  ? {
+                      transition: '1s opacity ease-in-out',
+                      transform: `translateX(0)`,
+                      opacity: 1,
+                    }
+                  : {}
+              }
+            >
+              <h2>Skills</h2>
+              <ul className="skills" ref={skillsWrapper}>
+                {skills.map((skill) => {
+                  return (
+                    <li className="skill-bar-wrapper" key={skill.skillName}>
+                      <div
+                        className="skill-bar"
+                        style={
+                          isVisibleSkillsWrapper
+                            ? {
+                                transition: `${
+                                  1 + skill.id / 10
+                                }s width ease-in-out`,
+                                width: width * (skill.amount / 100),
+                              }
+                            : {
+                                width: 1,
+                              }
+                        }
+                      ></div>
+                      <div className="skill-name" style={{ width: '40%' }}>
+                        {skill.skillName}
+                      </div>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )}
+        </IsVisible>
+      </div>
+    </Fade>
+  )
+}
+
+export default Skills
